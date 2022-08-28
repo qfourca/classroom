717d72990dba7e149f407a591a9419c5f339a62f
qfourca <qfourca3305@gmail.com>
2022-08-28 15:20:32 +0900

    upload

--- src/component/main/index.jsx
+++ src/component/main/index.jsx
@@ -2,26 +2,12 @@ import GoogleLogin from 'react-google-login'
 import * as S from './index.style'
 import oAuth from '../../store/oAuth' // atom으로 만든 전역상태
 import {useRecoilState} from 'recoil' // 훅 import
-import config from '../../../config.json'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Render = () => {
     const [getOAuth, setOAuth] = useRecoilState(oAuth); // 전역상태를 state로 만듦
     const [getClassrooms, setClassrooms] = useState([])
-    // useEffect(() => {
-    //     if(getOAuth != undefined) {
-    //         console.log(getOAuth)
-    //         axios.get(`https://classroom.googleapis.com/v1/courses?key=${''}`,
-    //         {headers: {
-    //             Authorization: 'Bearer ' + getOAuth.accessToken,
-    //             Accept: 'application/json'
-    //         }})
-    //         .then((result) => {
-    //             console.log(result)
-    //         })
-    //     }
-    // }, [getOAuth])
     const success = (result) => {
         setOAuth(result)
     }
@@ -31,26 +17,13 @@ const Render = () => {
     return (
         <>
             {
-                getOAuth == undefined? 
-                (
-                    <div>
-                        <GoogleLogin
-                            key={config.key}
-                            clientId={config.clientId}
-                            onSuccess={success}
-                            onFailure={fail}/>
-                    </div>
-                ):
-                <>
-                    {
-                        getClassrooms.map(element => {
-                            console.log(element)
-                            return <div></div>
-                        })
-                    }
-                </>
+                getClassrooms.map(element => {
+                    console.log(element)
+                    return <div></div>
+                })
             }
         </>
+
     )
 }
 export default Render
